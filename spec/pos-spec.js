682da93df604cf73a3f653f7557bb9cf7a331afe
Ni WANG <ni.wang@rea-group.com>
2014-11-06 00:13:48 +0800

    receipt for two items

--- spec/pos-spec.js
+++ spec/pos-spec.js
@@ -28,4 +28,20 @@ describe('POS', function(){
 
       expect(result).toBe(expectedResult);
   });
+
+  it('should print receipt for 1 coca cola and 1 instant noodle', function(){
+      var input = ['ITEM000000', 'ITEM000005'],
+      expectedResult = '***<没钱赚商店>购物清单***\n' +
+          '名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n' +
+          '名称：方便面，数量：1袋，单价：4.50(元)，小计：4.50(元)\n' +
+          '----------------------\n' +
+          '----------------------\n' +
+          '总计：7.50(元)\n' +
+          '节省：0.00(元)\n' +
+          '**********************';
+
+      var result = printInventory(input);
+
+      expect(result).toBe(expectedResult);
+  });
 });
--- src/pos.js
+++ src/pos.js
@@ -5,21 +5,36 @@ function printItemLineFor(item){
   '小计：' + item.price.toFixed(2) + '(元)\n'
 }
 
-function receiptFor(purchase){
-  return '***<没钱赚商店>购物清单***\n' + printItemLineFor(purchase) +
-      '----------------------\n' +
-      '----------------------\n' +
-      '总计：' + purchase.price.toFixed(2) + '(元)\n' +
-      '节省：0.00(元)\n' +
-      '**********************';
+function totalPriceOf(itemList){
+  return itemList.reduce(function(totalPrice, item){
+    return totalPrice += item.price;
+  }, 0);
+}
+
+function receiptFor(purchases){
+  var itemLines = purchases.reduce(function(itemLines, purchase){
+    return itemLines += printItemLineFor(purchase);
+  },'');
+
+  return  '***<没钱赚商店>购物清单***\n' +
+          itemLines +
+          '----------------------\n' +
+          '----------------------\n' +
+          '总计：' + totalPriceOf(purchases).toFixed(2) + '(元)\n' +
+          '节省：0.00(元)\n' +
+          '**********************';
 }
 
 function printInventory(input) {
   var allItems = loadAllItems(),
-      purchase = {};
-  allItems.forEach(function(item){
-    if(item.barcode == input) purchase = item;
+      purchases = [];
+
+  input.forEach(function(one){
+    allItems.forEach(function(item){
+      if(item.barcode == one) purchases.push(item);
+    });
   });
 
-  return receiptFor(purchase);
+
+  return receiptFor(purchases);
 };
