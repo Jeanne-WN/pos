ed69754ea7e786c4fb3b82fcd3cc23739e91e397
Ni WANG <ni.wang@rea-group.com>
2014-11-05 23:58:20 +0800

    another item

--- src/pos.js
+++ src/pos.js
@@ -1,9 +1,18 @@
-function printInventory() {
+function printInventory(input) {
+  var allItems = loadAllItems(),
+      purchase = {};
+  allItems.forEach(function(item){
+    if(item.barcode == input) purchase = item;
+  });
+
   return '***<没钱赚商店>购物清单***\n' +
-      '名称：可口可乐，数量：1个，单价：3.00(元)，小计：3.00(元)\n' +
+      '名称：' + purchase.name + '，' +
+      '数量：1' + purchase.unit + '，' +
+      '单价：' + purchase.price.toFixed(2) + '(元)，' +
+      '小计：' + purchase.price.toFixed(2) + '(元)\n' +
       '----------------------\n' +
       '----------------------\n' +
-      '总计：3.00(元)\n' +
+      '总计：' + purchase.price.toFixed(2) + '(元)\n' +
       '节省：0.00(元)\n' +
       '**********************';
 };
