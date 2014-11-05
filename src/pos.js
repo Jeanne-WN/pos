0601b58070b0db211bed6b64e597e7f4041f0535
Ni WANG <ni.wang@rea-group.com>
2014-11-06 00:01:32 +0800

    refactor

--- src/pos.js
+++ src/pos.js
@@ -1,3 +1,19 @@
+function printItemLineFor(item){
+  return '名称：' + item.name + '，' +
+  '数量：1' + item.unit + '，' +
+  '单价：' + item.price.toFixed(2) + '(元)，' +
+  '小计：' + item.price.toFixed(2) + '(元)\n'
+}
+
+function receiptFor(purchase){
+  return '***<没钱赚商店>购物清单***\n' + printItemLineFor(purchase) +
+      '----------------------\n' +
+      '----------------------\n' +
+      '总计：' + purchase.price.toFixed(2) + '(元)\n' +
+      '节省：0.00(元)\n' +
+      '**********************';
+}
+
 function printInventory(input) {
   var allItems = loadAllItems(),
       purchase = {};
@@ -5,14 +21,5 @@ function printInventory(input) {
     if(item.barcode == input) purchase = item;
   });
 
-  return '***<没钱赚商店>购物清单***\n' +
-      '名称：' + purchase.name + '，' +
-      '数量：1' + purchase.unit + '，' +
-      '单价：' + purchase.price.toFixed(2) + '(元)，' +
-      '小计：' + purchase.price.toFixed(2) + '(元)\n' +
-      '----------------------\n' +
-      '----------------------\n' +
-      '总计：' + purchase.price.toFixed(2) + '(元)\n' +
-      '节省：0.00(元)\n' +
-      '**********************';
+  return receiptFor(purchase);
 };
